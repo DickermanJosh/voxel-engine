5988d42b6eeed22795bb2ffcd6e6f9fe4b95f2bb
DickermanJosh <dickermanjosh@gmail.com>
2024-05-28 17:00:04 -0700

    square render

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,13 @@
 #include <glm/glm.hpp>
 #include <iostream>
 
+#include "ShaderProgram.hpp"
+
 int main() {
     sf::ContextSettings settings;
+    settings.depthBits = 24;
+    settings.stencilBits = 8;
+    settings.antialiasingLevel = 4;
     settings.majorVersion = 4;
     settings.minorVersion = 6;
 
@@ -20,13 +25,64 @@ int main() {
         return -1;
     }
 
-    while (window.isOpen()) {
+    ShaderProgram shaderProgram("../res/shaders/vertex.glsl", "../res/shaders/fragment.glsl");
 
-        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    // ============= Square rendering ===========
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // bind the VAO fist
+    glBindVertexArray(VAO);
+
+    // Bind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Bind EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Pos attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    // Unbind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Unbind VAO (Keep EBO binding)
+    glBindVertexArray(0);
+
+
+    // Game loop
+    while (window.isOpen()) {
+        glClearColor(0.0f, 0.3f, 0.6f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        shaderProgram.use();
+        glBindVertexArray(VAO);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+        
         window.display();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     return 0;
 }
