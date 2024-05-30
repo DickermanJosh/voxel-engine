07c4be3330c5b80d2f1186de89a3235faf910e92
DickermanJosh <dickermanjosh@gmail.com>
2024-05-30 14:35:58 -0700

    Backface culling & fixed render order to be clockwise

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,13 @@
 #include <iostream>
 
 #include "ShaderProgram.hpp"
-#include "Mesh.hpp"
+#include "Block.hpp"
 #include "Camera.hpp"
 #include "CameraController.hpp"
 #include "EventHandler.hpp"
 
 int main() {
-  sf::ContextSettings settings;
+    sf::ContextSettings settings;
     settings.depthBits = 24;
     settings.stencilBits = 8;
     settings.antialiasingLevel = 4;
@@ -25,6 +25,7 @@ int main() {
     sf::Window window(sf::VideoMode(2560, 1440), "Minecraft", sf::Style::Default, settings);
     window.setActive(true);
     window.setMouseCursorVisible(false);
+    window.setVerticalSyncEnabled(true);
 
     GLenum err = glewInit();
     if (err != GLEW_OK) {
@@ -35,47 +36,91 @@ int main() {
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
 
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CW); // Count-clockwise winding order for front faces
+
     ShaderProgram shaderProgram("../res/shaders/vertex.glsl", "../res/shaders/fragment.glsl");
 
     // Floor vertices and indices
     std::vector<float> floorVertices = {
         // positions
-        -5.0f,  0.0f, -5.0f,
-         5.0f,  0.0f, -5.0f,
-         5.0f,  0.0f,  5.0f,
-        -5.0f,  0.0f,  5.0f
+        -1.0f,  0.0f, -1.0f,
+        -1.0f,  0.0f,  1.0f,
+        1.0f,  0.0f,  1.0f,
+        1.0f,  0.0f, -1.0f
     };
     std::vector<unsigned int> floorIndices = {
         0, 1, 2, 2, 3, 0
     };
 
-    // Wall vertices and indices (Negative Z)
-    std::vector<float> wallZVertices = {
+    // Ceiling vertices and indices
+    std::vector<float> ceilingVertices = {
         // positions
-        -5.0f,  0.0f, -5.0f,
-         5.0f,  0.0f, -5.0f,
-         5.0f,  5.0f, -5.0f,
-        -5.0f,  5.0f, -5.0f
+        -1.0f,  1.0f, -1.0f,
+        1.0f,  1.0f, -1.0f,
+        1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f
     };
-    std::vector<unsigned int> wallZIndices = {
+    std::vector<unsigned int> ceilingIndices = {
+        0, 1, 2, 2, 3, 0
+    };
+
+    // Wall vertices and indices (Negative Z)
+    std::vector<float> negWallZVertices = {
+        // positions
+        -1.0f,  0.0f, -1.0f, // bottom-left
+        1.0f,  0.0f, -1.0f, // bottom-right
+        1.0f,  1.0f, -1.0f, // top-right
+        -1.0f,  1.0f, -1.0f // top-left
+    };
+    std::vector<unsigned int> negWallZIndices = {
+        0, 1, 2, 2, 3, 0
+    };
+
+    // Wall vertices and indices (Positive Z)
+    std::vector<float> posWallZVertices = {
+        // positions
+        -1.0f,  0.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  0.0f,  1.0f,
+    };
+    std::vector<unsigned int> posWallZIndices = {
         0, 1, 2, 2, 3, 0
     };
 
     // Wall vertices and indices (Negative X)
-    std::vector<float> wallXVertices = {
+    std::vector<float> negWallXVertices = {
         // positions
-        -5.0f,  0.0f, -5.0f,
-        -5.0f,  0.0f,  5.0f,
-        -5.0f,  5.0f,  5.0f,
-        -5.0f,  5.0f, -5.0f
+        -1.0f,  0.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  0.0f,  1.0f,
     };
-    std::vector<unsigned int> wallXIndices = {
+    std::vector<unsigned int> negWallXIndices = {
+        0, 1, 2, 2, 3, 0
+    };
+
+    // Wall vertices and indices (Positive X)
+    std::vector<float> posWallXVertices = {
+        // positions
+        1.0f,  0.0f, -1.0f,
+        1.0f,  0.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f, -1.0f,
+    };
+    std::vector<unsigned int> posWallXIndices = {
         0, 1, 2, 2, 3, 0
     };
 
     Mesh floor(floorVertices, floorIndices);
-    Mesh wallZ(wallZVertices, wallZIndices);
-    Mesh wallX(wallXVertices, wallXIndices);
+    Mesh ceiling(ceilingVertices, ceilingIndices);
+    Mesh posWallZ(posWallZVertices, posWallZIndices);
+    Mesh negWallZ(negWallZVertices, negWallZIndices);
+    Mesh posWallX(posWallXVertices, posWallXIndices);
+    Mesh negWallX(negWallXVertices, negWallXIndices);
+    // Block block(glm::vec3(0.0f, 2.0f, 15.0f));
 
     // Camera Setup
     glm::vec3 cameraPos = glm::vec3(0.0f, 1.0f, 3.0f);
@@ -118,27 +163,58 @@ int main() {
         shaderProgram.setUniform("projection", projection);
         shaderProgram.setUniform("view", view);
         shaderProgram.setUniform("transform", floorTransform);
+        // Floor - Red
         shaderProgram.setUniform("color", glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
         floor.draw();
+
+        // Render the ceiling
+        glm::mat4 ceilingTransform = glm::mat4(1.0f);
+        shaderProgram.setUniform("transform", ceilingTransform);
+        // Ceiling - White
+        shaderProgram.setUniform("color", glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+        ceiling.draw();
+
+        // Render the wall in the positive Z direction
+        glm::mat4 posWallZTransform = glm::mat4(1.0f);
+        shaderProgram.setUniform("transform", posWallZTransform);
+        // Positive Z - Yellow
+        shaderProgram.setUniform("color", glm::vec4(1.0f, 1.0f, 0.0f, 1.0f));
+        posWallZ.draw();
 
         // Render the wall in the negative Z direction
         glm::mat4 wallZTransform = glm::mat4(1.0f);
         shaderProgram.setUniform("transform", wallZTransform);
+        // Negative Z - Green
         shaderProgram.setUniform("color", glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
-        wallZ.draw();
+        negWallZ.draw();
+
+        // Render the wall in the positive X direction
+        glm::mat4 posWallXTransform = glm::mat4(1.0f);
+        shaderProgram.setUniform("transform", posWallXTransform);
+        // Positive X - Purple
+        shaderProgram.setUniform("color", glm::vec4(1.0f, 0.0f, 1.0f, 1.0f));
+        posWallX.draw();
 
         // Render the wall in the negative X direction
         glm::mat4 wallXTransform = glm::mat4(1.0f);
         shaderProgram.setUniform("transform", wallXTransform);
+        // Negative X - Blue
         shaderProgram.setUniform("color", glm::vec4(0.0f, 0.0f, 1.0f, 1.0f));
-        wallX.draw();
+        negWallX.draw();
 
+
+        // glm::mat4 transform = glm::mat4(1.0f);
+        // shaderProgram.setUniform("projection", projection);
+        // shaderProgram.setUniform("view", view);
+        // shaderProgram.setUniform("transform", transform);
+        // shaderProgram.setUniform("color", glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+        // block.draw();
         // Center the mouse cursor
         sf::Mouse::setPosition(center, window);
 
         window.display();
 
-        std::cout << "Camera Pos: " << glm::to_string(camera.getPosition()) << std::endl;
+        // std::cout << "Camera Pos: " << glm::to_string(camera.getPosition()) << std::endl;
     }
 
     return 0;
